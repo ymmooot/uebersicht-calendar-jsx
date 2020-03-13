67539fa9a9feb8b71347d51fd99f29ed09254d3f
ryota yamamoto <ryota-yamamoto@retty.me>
2020-03-13 23:40:19 +0900

    format code

--- simple-calenda.widget/simple-calendar.jsx
+++ simple-calenda.widget/simple-calendar.jsx
@@ -1,50 +1,58 @@
-import { css } from "uebersicht"
+import { css } from "uebersicht";
 
 const sundayFirstCalendar = 'cal -h && date "+|%-d"';
 export const command = sundayFirstCalendar;
 export const refreshFrequency = 3600000; // ms
 export const className = `
   font-family: Source Han Code JP, Helvetica Neue;
+  font-size: 16px;
   color: #fff;
   top: 10px;
   left: 40px;
-`
-const rect = '30px';
-const baseFontSize = '16px';
-const margin = '8px';
+`;
+const rect = "30px";
+const margin = "8px";
 
 const parse = output => {
-  const separated = output.split('|')
-  const rows = separated[0].trim().split('\n')
-  const today = separated[1].trim()
+  if (!output) {
+    return null;
+  }
+
+  const separated = output.split("|");
+  const rows = separated[0].trim().split("\n");
+  const today = separated[1].trim();
   return {
-    headers: rows[0].split(' ').slice(0, 2),
-    tableHeaderRow: rows[1].trim().split(' '),
+    headers: rows[0].split(" ").slice(0, 2),
+    tableHeaderRow: rows[1].trim().split(" "),
     tableBodyRows: rows.slice(2).map(s => s.match(/.{3}|.{2}$/g)),
-    today,
-   }
-}
+    today
+  };
+};
 
 //////////////// header ////////////////
 const headerCss = css`
   padding-left: 5px;
-  font-size: ${baseFontSize} * 1.4em;
+  font-size: 2rem;
   margin-bottom: 10px;
-`
-const header = (month, year) => <h1 className={headerCss}>{month} <span>{year}</span></h1>
+`;
+const header = (month, year) => (
+  <h1 className={headerCss}>
+    {month} <span>{year}</span>
+  </h1>
+);
 
 //////////////// table ////////////////
 const tableCss = css`
   border-collapse: collapse;
   table-layout: fixed;
-  font-size: ${baseFontSize};
+  font-size: 1rem;
 
   th {
     display: inline-block;
     width: ${rect};
     height: ${rect};
     text-align: center;
-    font-size: ${baseFontSize};
+    font-size: 1rem;
     font-weight: bold;
   }
   th:not(:first-of-type) {
@@ -62,50 +70,53 @@ const tableCss = css`
   td:not(:first-of-type) {
     margin-left: ${margin};
   }
-`
-const todayDecoCss = css`
-  display: block;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: #fff;
-  opacity: 0.3;
-  border-radius: 50%;
-`
+`;
 
 const todayCss = css`
   position: relative;
   font-weight: bold;
   text-shadow: 1px 1px #555;
-`
+  background-color: rgba(255, 255, 255, 0.3);
+  border-radius: 50%;
+`;
 
 const table = (headers, bodies, today) => {
-  return <table className={tableCss}>
-    <thead>
-      <tr>
-        { headers.map(s => <th>{s}</th>) }
-      </tr>
-    </thead>
-    <tbody>
-      { bodies.map(row => 
+  return (
+    <table className={tableCss}>
+      <thead>
         <tr>
-          { row.map(s=> {
-            return s.trim() === today ? <td className={todayCss}><span className={todayDecoCss}/>{s}</td> : <td>{s}</td> 
-          })}
-        </tr>) 
-      }
-    </tbody>
-  </table>
-}
+          {headers.map(s => (
+            <th>{s}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {bodies.map(row => (
+          <tr>
+            {row.map(s => (
+              <td className={s.trim() === today ? todayCss : ""}>{s}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
-export const render = ({output, error}) => {
-  const s = parse(output)
-  return error ? <p>{error}</p> : (
+export const render = ({ output, error }) => {
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!output) {
+    return <p>output is not defined</p>;
+  }
+
+  const s = parse(output);
+  return (
     <div>
       {header(...s.headers)}
       {table(s.tableHeaderRow, s.tableBodyRows, s.today)}
     </div>
   );
-}
+};
